f4ded57d26f87dca40ee2c8c81b8b20144e4ff14
Jose Ramirez <josemramirez81@outlook.com>
2022-12-07 22:11:28 -0500

    Bridge the communication between front and backend

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -6,9 +6,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace('/api', '')
+      }
+    }
+  },
+    resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+
 })
