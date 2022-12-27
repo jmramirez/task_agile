7869c32b08be09ddb3bc6ff4475965ba255c6278
Jose Ramirez <josemramirez81@outlook.com>
2022-12-27 14:51:44 -0500

    Create HomePage with route

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,10 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LoginPage from "@/views/LoginPage.vue";
 import RegisterPage from "@/views/RegisterPage.vue";
+import HomePage from "@/views/HomePage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'HomePage',
+      component: HomePage
+    },
     {
       path: '/login',
       name: 'LoginPage',
